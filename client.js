b3d4040340f72700c336ce3a99af4b7590118c94
Anaaag <agarc20@outlook.com>
2022-08-15 14:40:36 -0700

    created client.js

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+
+// establishes a connection with the game server
+const connect = function (data) {
+  console.log("connecting...")
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    console.log("connected to server")
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says", data)
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+
+  return conn;
+};
+
+module.exports = {connect}
